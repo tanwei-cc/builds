006af8e20daa34e7409fff7ee1851423181b3cb3
tw <tanwei_yx@126.com>
2022-06-22 18:53:52 +0800

    first commit

---
+++ scripts/release.js
@@ -0,0 +1,17 @@
+// import { Octokit, App } from "octokit";
+
+// const octokit = new Octokit({
+//   auth: 'personal-access-token123'
+// });
+
+// await octokit.request('POST /repos/{owner}/{repo}/releases', {
+//   owner: 'OWNER',
+//   repo: 'REPO',
+//   tag_name: 'v1.0.0',
+//   target_commitish: 'master',
+//   name: 'v1.0.0',
+//   body: 'Description of the release',
+//   draft: false,
+//   prerelease: false,
+//   generate_release_notes: false
+// });
